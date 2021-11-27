41df92ddd17ebefa775ed2082f4bbe83402a9c84
Jeramy Sena <jeramy_sena@hotmail.com>
2021-11-27 16:29:57 -0600

    saving some final touches

--- src/page-template.js
+++ src/page-template.js
@@ -5,7 +5,7 @@ const generateManager = function (manager) {
         <div class="card employee-card">
   <div class="card-header bg-primary">
       <h2 class="card-title text-white">${manager.name}</h2>
-      <h3 class="card-title text-white"><i class="fa-light fa-mug-hot"></i> Manager</h3>
+      <h3 class="card-title text-white"><i class="far fa-clipboard"></i> Manager</h3>
   </div>
   <div class="card-body">
       <ul class="list-group">
@@ -42,7 +42,7 @@ const generateIntern = function (intern) {
      <div class="card employee-card">
   <div class="card-header bg-primary">
       <h2 class="card-title text-white">${intern.name}</h2>
-      <h3 class="card-title text-white"><i class="fa-light fa-user-graduate"></i> Intern</h3>
+      <h3 class="card-title text-white"><i class="fas fa-pencil-alt"></i> Intern</h3>
   </div>
   <div class="card-body">
       <ul class="list-group">
@@ -57,25 +57,25 @@ const generateIntern = function (intern) {
 
 generatePage = (data) => {
   // array for cards
-  pageArray = [];
+  cardArray = [];
   for (let i = 0; i < data.length; i++) {
     const employee = data[i];
     const role = employee.getRole();
     if (role === "Manager") {
       const managerCard = generateManager(employee);
-      pageArray.push(managerCard);
+      cardArray.push(managerCard);
     }
     if (role === "Engineer") {
       const engineerCard = generateEngineer(employee);
-      pageArray.push(engineerCard);
+      cardArray.push(engineerCard);
     }
     if (role === "Intern") {
       const internCard = generateIntern(employee);
-      pageArray.push(internCard);
+      cardArray.push(internCard);
     }
   }
 
-  const employeeCards = pageArray.join("");
+  const employeeCards = cardArray.join("");
   const generateTeam = generateTeamPage(employeeCards);
   return generateTeam;
 };
@@ -91,7 +91,7 @@ const generateTeamPage = function (teamMemberCards) {
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Team Profile</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
   
   <link rel="stylesheet" href="./src/style.css">
